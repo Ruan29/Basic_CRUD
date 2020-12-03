e383f5a032413cf0529df52c2eff425ac5b4852b
Ruan Lima <ruan_a.29@hotmail.com>
2020-12-03 05:56:40 -0300

    Initial commit

---
+++ crud basico/index.php
@@ -0,0 +1,36 @@
+<?php
+include_once 'includes/header.php';
+?>
+
+<?php
+include_once 'includes/footer.php';
+?>
+
+<div class="row">
+  <div class="col s12 m6 push-m3">
+    <h3 class="light">Clientes</h3>
+    <table class="stripped">
+      <thead>
+        <tr>
+          <th>Nome:</th>
+          <th>Sobrenome:</th>
+          <th>Email:</th>
+          <th>Idade:</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>João</td>
+          <td>Oliveira</td>
+          <td>joaooliveira@gmail.com</td>
+          <td>23</td>
+          <td><a href="" class="btn-floating orange"></a></td>
+          <td><a href="" class="btn-floating orange"></a></td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
+    <a href="" class="btn">Adicionar cliente</a>
+  </div>
+</div>
